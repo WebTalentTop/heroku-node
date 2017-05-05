5dce1e98f7b80044d4667ad5f35d258d7d00a50e
LV <lv@LVs-MacBook-Pro.local>
2017-05-06 04:20:55 +0800

    ss

--- server.js
+++ server.js
@@ -71,20 +71,20 @@ sockserver.installHandlers(server, {prefix:'/sockserver'});
 server.listen(3000, '0.0.0.0'); 
 
 
-// var port = process.env.PORT || 8080;
-// // set the view engine to ejs
-// app.set('view engine', 'ejs');
+var port = process.env.PORT || 8080;
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-// // make express look in the public directory for assets (css/js/img)
-// app.use(express.static(__dirname + '/public'));
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
 
-// // set the home page route
-// app.get('/', function(req, res) {
+// set the home page route
+app.get('/', function(req, res) {
 
-// 	// ejs render automatically looks in the views folder
-// 	res.render('index');
-// });
+	// ejs render automatically looks in the views folder
+	res.render('index');
+});
 
-// app.listen(port, function() {
-// 	console.log('Our app is running on http://localhost:' + port);
-// });
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
