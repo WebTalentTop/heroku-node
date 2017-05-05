7eec323fe81c4197d5af52769a48d0bcf8ddeea6
LV <lv@LVs-MacBook-Pro.local>
2017-05-05 17:29:19 +0800

    change

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ var app = express();
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
-
+////////////////////
+var http = require('http'),
+    sockjs = require('sockjs'),
+    sockserver = sockjs.createServer(),
+    connections = [];
+var http1 =require('http').Server(app);
+var mysql      = require('mysql');
+var io = require('socket.io')(http1);
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -21,3 +28,58 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 });
+var io = require('socket.io')(http1);
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg_total);
+    });
+});
+var db_connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : 'mysql',
+  database : 'stateDB'
+});
+db_connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");   
+} else {
+    console.log("Error connecting database ... nn");    
+}
+});
+
+    
+
+sockserver.on('connection', function(conn) {
+  console.log('Connected');
+  msg_total+='Connected';
+  db_connection.query('INSERT INTO stateTable (message) VALUES ("Connected")', function(err, rows, fields) {
+
+    });
+  connections.push(conn);
+  conn.on('data', function(msg) {
+    console.log('Message: ' + msg);
+    msg_total+=msg;
+    io.sockets.emit('chat message', msg);
+    // send the message to all clients
+    for (var i=0; i < connections.length; ++i) {
+      connections[i].write(msg);
+    }
+    db_connection.query('INSERT INTO stateTable (message) VALUES (?)', msg, function(err, rows, fields) {
+
+    });
+    
+  });
+  conn.on('close', function() {
+    connections.splice(connections.indexOf(conn), 1); // remove the connection
+    console.log('Disconnected');
+    msg_total+='Disconnected';
+    db_connection.query('INSERT INTO stateTable (message) VALUES ("Disconnected")', function(err, rows, fields) {
+      db_connection.end();
+    });
+  });
+});
+
+var server = http.createServer();
+sockserver.installHandlers(server, {prefix:'/sockserver'});
+server.listen(3000, '0.0.0.0');
